5429713e64d2a445ce01fdeaa3d3bcfbc1a91e3a
jwoods7097 <43973305+jwoods7097@users.noreply.github.com>
2019-01-25 16:10:59 -0600

    Fixed to use only one motor controller
    Accidentally added an extra motor controller when it was not needed.

--- src/main/java/org/usfirst/frc/team5968/robot/Launcher.java
+++ src/main/java/org/usfirst/frc/team5968/robot/Launcher.java
@@ -8,16 +8,14 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 public class Launcher implements ILauncher
 {
 
-    private TalonSRX leftMotor;
-    private TalonSRX rightMotor;
+    private TalonSRX launcherMotor;
 
     private double motorSpeed;
     private static final double HIGH = 0.9;
     private static final double LOW = 0.0;
 
     public Launcher() {
-        leftMotor = new TalonSRX(PortMap.portOf(CAN.LAUNCHER_LEFT_MOTOR_CONTROLLER));
-        rightMotor = new TalonSRX(PortMap.portOf(CAN.LAUNCHER_RIGHT_MOTOR_CONTROLLER));
+        launcherMotor = new TalonSRX(PortMap.portOf(CAN.LAUNCHER_MOTOR_CONTROLLER));
 
         stop();
     }
@@ -39,7 +37,6 @@ public class Launcher implements ILauncher
 
     @Override
     public void periodic() {
-        leftMotor.set(ControlMode.PercentOutput, motorSpeed);
-        rightMotor.set(ControlMode.PercentOutput, motorSpeed);
+        launcherMotor.set(ControlMode.PercentOutput, motorSpeed);
     }
 }
