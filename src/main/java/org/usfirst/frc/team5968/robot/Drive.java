b45ab496ce71df7b62aedc14385496f085ad9693
allen <allenyz458@gmail.com>
2019-01-29 16:07:57 -0600

    added methods

--- src/main/java/org/usfirst/frc/team5968/robot/Drive.java
+++ src/main/java/org/usfirst/frc/team5968/robot/Drive.java
@@ -1,11 +1,18 @@
+package org.usfirst.frc.team5968.robot; 
 import java.lang.Object;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Talon;
 
-public class Drive implements IDrive{
-    
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+public class Drive implements IDrive {
+
     private GenericHID xbox;
-    private SendableBase encoder;
+    private NavXMXP navX;
 
     private TalonSRX leftMotorControllerLead;
     private TalonSRX leftMotorControllerFollow;
@@ -14,39 +21,65 @@ public class Drive implements IDrive{
     private TalonSRX middleMotorControllerLead;
     private TalonSRX middleMotorControllerFollow;
 
+    private double xleftJoystick;
+    private double yleftJoystick;
+    private double robotAngle;
+    private double joyStickAngle;
+    private double robotDriveAngle;
+    private double robotXDriveDirection;
+    private double robotYDriveDirection;
+    private double robotSpeed;
 
-    @Override
-    public void driveManual(){
+    public Drive(){
 
-        double xbox = new XboxController(XBOXPORT);
-        double navX = new AHRS(AHRSPORT);
+        xbox = new XboxController(XBOXPORT);
 
-        double leftMotorControllerLead = new TalonSRX(LEFT_MOTOR_CONTROLLER_LEAD_PORT);
-        double leftMotorControllerFollow = new TalonSRX(LEFT_MOTOR_CONTROLLER_FOLLOW_PORT);
-        double rightMotorControllerLead = new TalonSRX(RIGHT_MOTOR_CONTROLLER_LEAD_PORT);
-        double rightMotorControllerFollow = new TalonSRX(RIGHT_MOTOR_CONTROLLER_FOLLOW_PORT);
-        double middleMotorControllerLead = new TalonSRX(MIDDLE_MOTOR_CONTROLLER_LEAD_PORT);
-        double middleMotorControllerFollow = new TalonSRX(MIDDLE_MOTOR_CONTROLLER_FOLLOW_PORT);
+        leftMotorControllerLead = new TalonSRX(LEFT_MOTOR_CONTROLLER_LEAD_PORT);
+        leftMotorControllerFollow = new TalonSRX(LEFT_MOTOR_CONTROLLER_FOLLOW_PORT);
+        rightMotorControllerLead = new TalonSRX(RIGHT_MOTOR_CONTROLLER_LEAD_PORT);
+        rightMotorControllerFollow = new TalonSRX(RIGHT_MOTOR_CONTROLLER_FOLLOW_PORT);
+        middleMotorControllerLead = new TalonSRX(MIDDLE_MOTOR_CONTROLLER_LEAD_PORT);
+        middleMotorControllerFollow = new TalonSRX(MIDDLE_MOTOR_CONTROLLER_FOLLOW_PORT);
 
         leftMotorControllerFollow.follow(leftMotorControllerLead);
         rightMotorControllerFollow.follow(rightMotorControllerLead);
         middleMotorControllerFollow.follow(middleMotorControllerLead);
+    
+    }
 
-        double xleftJoystick = xbox.getX(kLeft);
-        double yleftJoystick = xbox.getY(kLeft);
-        double robotSpeed = Math.sqrt(Math.pow(xleftJoystick, 2) + Math.pow(yleftJoystick, 2));
+    @Override
+    private void setRobotSpeed(){
 
-        double robotAngle = navX.getYaw();
-        double joyStickAngle = Math.atan(xleftJoystick / yleftJoystick);
-        double robotDriveAngle = joyStickAngle + robotAngle;    //angle relative to robot
+        robotSpeed = Math.sqrt(Math.pow(xleftJoystick, 2) + Math.pow(yleftJoystick, 2));
 
-        double robotXDriveDirection = 1 / Math.sqrt(1 + Math.pow(Math.tan(robotDriveAngle), 2));
-        double robotYDriveDirection = Math.tan(robotDriveAngle) / Math.sqrt(1 + Math.pow(Math.tan(robotDriveAngle), 2));
+    }
+
+    @Override
+    private void getRobotAngle(){
+
+        robotAngle = navX.getYaw();
+
+    }
+
+    @Override
+    private void robotDriveAngle(){
+
+        xleftJoystick = xbox.getX(kLeft);
+        yleftJoystick = xbox.getY(kRight);
+        joyStickAngle = Math.atan(xleftJoystick / yleftJoystick);
+        robotDriveAngle = joyStickAngle + robotAngle;    //angle relative to robot
+        robotXDriveDirection = 1 / Math.sqrt(1 + Math.pow(Math.tan(robotDriveAngle), 2));
+        robotYDriveDirection = Math.tan(robotDriveAngle) / Math.sqrt(1 + Math.pow(Math.tan(robotDriveAngle), 2));
+
+    }
+
+    @Override
+    private void setRobotSpeed(){
 
         leftMotorControllerLead.set(robotSpeed * robotYDriveDirection);
         rightMotorControllerLead.set(robotSpeed * robotYDriveDirection);
         middleMotorControllerLead.set(robotSpeed * robotXDriveDirection);
-    
+
     }
 
 }
