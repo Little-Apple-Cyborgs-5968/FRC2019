90fed454868c4d0f765b2c1cd110be1f1386077e
jwoods7097 <43973305+jwoods7097@users.noreply.github.com>
2019-01-29 17:23:45 -0600

    Merge fix

--- src/main/java/org/usfirst/frc/team5968/robot/PortMap.java
+++ src/main/java/org/usfirst/frc/team5968/robot/PortMap.java
@@ -33,11 +33,11 @@ public class PortMap {
         switch(canDevice) {
         case LEFT_MOTOR_CONTROLLER_LEAD:
             return 2;
-        case LEFT_MOTOR_CONTROLLER_FOLLOW:
+        case LEFT_MOTOR_CONTROLLER_FOLLOWER:
             return 3;
         case RIGHT_MOTOR_CONTROLLER_LEAD:
             return 4;
-        case RIGHT_MOTOR_CONTROLLER_FOLLOW:
+        case RIGHT_MOTOR_CONTROLLER_FOLLOWER:
             return 5;
         case LAUNCHER_MOTOR_CONTROLLER: 
             return 1;
