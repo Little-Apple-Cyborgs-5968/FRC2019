a69451ddb4388c6736b59ea76ea0a39f1b544ab5
jwoods7097 <jwoods7097@gmail.com>
2019-02-02 14:50:28 -0600

    Created CargoGuide.java

---
+++ src/main/java/org/usfirst/frc/team5968/robot/CargoGuide.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team5968.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class CargoGuide implements ICargoGuide {
+
+    private DoubleSolenoid guidePiston;
+    private PistonState pistonState; 
+
+    public CargoGuide() {
+
+        guidePiston = new DoubleSolenoid(2, 3); // add real channels later
+        pistonState = PistonState.OPEN;
+
+    }
+
+    @Override
+    public void engageGuide() {
+        grabberPiston.set(DoubleSolenoid.Value.kForward);
+    }
+
+    @Override
+    public void disengageGuide() {
+        grabberPiston.set(DoubleSolenoid.Value.kReverse);
+    }
+
+}
