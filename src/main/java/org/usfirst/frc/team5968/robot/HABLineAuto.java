ce8e819c95cd5d5b4187d2663b7e6641ba7e65cc
jwoods7097 <jwoods7097@gmail.com>
2019-02-02 16:28:31 -0600

    Fixed drive & formatting

--- src/main/java/org/usfirst/frc/team5968/robot/HABLineAuto.java
+++ src/main/java/org/usfirst/frc/team5968/robot/HABLineAuto.java
@@ -4,47 +4,40 @@ import org.usfirst.frc.team5968.Drive;
 
 public class HABLineAuto implements IRobotMode {
 
-    private IDrive drive; 
+    private IDrive drive;
 
     private double robotSpeed;
     private static final double HIGH = 0.5;
     private static final double LOW = 0.0;
 
     public HABLineAuto(IDrive drive) {
-        
         this.drive = drive; 
 
+        robotSpeed = LOW;
     }
 
-@Override
-public void init() {
+    @Override
+    public void init() {
+        robotSpeed = HIGH;
+        driveStraight(); 
+    }
 
-    robotSpeed = LOW;
-    driveStraight(); 
-    
-}
+    @Override
+    public void periodic() {
+        if(crossedLine()) {
+            drive.driveDistance(0, robotSpeed, 12.0, completionRoutine); // add real completion routine later
+            robotSpeed = LOW;
+        }  
+    }
 
-@Override
-public void periodic() {
-    // Visual sensor checks whether or not robot has crossed HAB line
-    if(crossedLine()) {
-        // if true: go forward a bit more, then stop (to ensure robot has crossed line)
-        drive.driveDistance(0, robotSpeed, 12.0, /* Runnable? */);
-        robotSpeed = LOW;
-    }  
-        // if false: keep going forward  
-}
+    public void driveStraight() {
+        drive.driveManual(0, robotSpeed);
+    }
 
-//crosses HAB line
-public void driveStraight() {
-    // Just needs to set motors going forward
-    drive.driveManual(0, robotSpeed);
-}
-
-private boolean crossedLine() {
-    // checks whether robot has crossed line.
-        // returns false if visual sensor detects carpet
-        // returns true if visual sensor detects HAB line
-}
+    private boolean crossedLine() {
+        // checks whether robot has crossed line.
+            // returns false if visual sensor detects carpet
+            // returns true if visual sensor detects HAB line
+    }
 
 }
