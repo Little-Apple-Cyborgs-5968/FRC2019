424fb595ee6c672f5a834f67bc3b013e875204df
Alice <alicetranghnle@gmail.com>
2019-02-12 17:21:49 -0600

    added hook from Chris

--- src/main/java/org/usfirst/frc/team5968/robot/Hook.java
+++ src/main/java/org/usfirst/frc/team5968/robot/Hook.java
@@ -1,23 +1,39 @@
-package org.usfirst.frc.team5968.robot;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-public class Hook implements IHook{
+public class Hook implements IHook {
+    
+    private DoubleSolenoid piston;
+    private PistonState pistonState; 
 
-    public Hook(){
-
+    
+    public Hook (){
+        piston = new DoubleSolenoid(0, 1); // change to real ports later
+        pistonState = PistonState.OPEN;
     }
 
     @Override
-    public void releasePanel(){
-        
+    public void grabPanel() {
+        pistonState = PistonState.CLOSED;
     }
 
     @Override
-    public void grabPanel(){
-        
+    public void releasePanel() {
+        pistonState = PistonState.OPEN;
     }
 
     @Override
-    public void toggleGrabbing(){
-        
+    public void toggleGrabbing() {
+        if (pistonState == PistonState.CLOSED) {
+            releasePanel();
+        } else {
+            grabPanel();
+        }
+    }
+    public void periodic() {
+        if (pistonState == PistonState.CLOSED) {
+            piston.set(DoubleSolenoid.Value.kReverse);
+        } else {
+            piston.set(DoubleSolenoid.Value.kForward);
+        }
     }
 }
