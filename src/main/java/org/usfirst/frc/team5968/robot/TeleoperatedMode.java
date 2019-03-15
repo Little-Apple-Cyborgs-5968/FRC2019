8237565de2e142efd3edccc80c955242e9799133
alicetranghnle <31904103+alicetranghnle@users.noreply.github.com>
2019-03-15 09:22:33 -0500

    Update TeleoperatedMode.java

--- src/main/java/org/usfirst/frc/team5968/robot/TeleoperatedMode.java
+++ src/main/java/org/usfirst/frc/team5968/robot/TeleoperatedMode.java
@@ -1,94 +1,185 @@
 package org.usfirst.frc.team5968.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class TeleoperatedMode implements IRobotMode {
+public class Drive implements IDrive {
 
-    private XboxController xboxController;
-    private IDrive drive;
-    private IHook hook;
-    private ILauncher launcher;
-    private ICargoGuide cargoGuide;
+    private IGyroscopeSensor gyroscope;
+    private DriveMode driveMode;
 
-    private boolean headingIsMaintained = true;
+    private TalonSRX leftMotorControllerLead;
+    private TalonSRX leftMotorControllerFollow;
+    private TalonSRX rightMotorControllerLead;
+    private TalonSRX rightMotorControllerFollow;
+    private TalonSRX middleMotorControllerLead;
+    private TalonSRX middleMotorControllerFollow;
 
-    private static final double TOLERANCE = 0.1 * 5.0;
-    private static final double ROTATION_SPEED_THRESHOLD = 0.3;
-    private static final double CONTROL_EXPONENT = 2.0;
+    private Runnable currentCompletionRoutine;
 
-    public TeleoperatedMode(IDrive drive, IHook hook, ILauncher launcher, ICargoGuide cargoGuide) {
+    private double xDirectionSpeed;
+    private double yDirectionSpeed;
+    private double desiredAngle;
+    private double rotationSpeed;
 
-        xboxController = new XboxController(PortMap.USB.XBOXCONTROLLER);
+    private static final double DELTA_ANGLE_SPEED_POWER = 1;
+    private static final double MAINTAINING_HEADING_SPEED = 1.0;
 
-        this.drive = drive;
-        this.hook = hook;
-        this.launcher = launcher;
-        this.cargoGuide = cargoGuide;
+    public Drive(IGyroscopeSensor gyroscope){
+
+        this.gyroscope = gyroscope;
+
+        leftMotorControllerLead = new TalonSRX(PortMap.CAN.LEFT_MOTOR_CONTROLLER_LEAD);
+        leftMotorControllerFollow = new TalonSRX(PortMap.CAN.LEFT_MOTOR_CONTROLLER_FOLLOW);
+        rightMotorControllerLead = new TalonSRX(PortMap.CAN.RIGHT_MOTOR_CONTROLLER_LEAD);
+        rightMotorControllerFollow = new TalonSRX(PortMap.CAN.RIGHT_MOTOR_CONTROLLER_FOLLOW);
+        middleMotorControllerLead = new TalonSRX(PortMap.CAN.MIDDLE_MOTOR_CONTROLLER_LEAD);
+        middleMotorControllerFollow = new TalonSRX(PortMap.CAN.MIDDLE_MOTOR_CONTROLLER_FOLLOW);
+
+        leftMotorControllerLead.setNeutralMode(NeutralMode.Brake);
+        leftMotorControllerFollow.setNeutralMode(NeutralMode.Brake);
+        middleMotorControllerLead.setNeutralMode(NeutralMode.Brake);
+        middleMotorControllerFollow.setNeutralMode(NeutralMode.Brake);
+        rightMotorControllerLead.setNeutralMode(NeutralMode.Brake);
+        rightMotorControllerFollow.setNeutralMode(NeutralMode.Brake);
+
+        leftMotorControllerFollow.setInverted(false);
+        leftMotorControllerLead.setInverted(false);
+        middleMotorControllerFollow.setInverted(true);
+        middleMotorControllerLead.setInverted(true);
+        rightMotorControllerLead.setInverted(true);
+        rightMotorControllerFollow.setInverted(true);
+
+        leftMotorControllerFollow.follow(leftMotorControllerLead);
+        rightMotorControllerFollow.follow(rightMotorControllerLead);
+        middleMotorControllerFollow.follow(middleMotorControllerLead);
+
+    }
+
+    @Override
+    public DriveMode getCurrentDriveMode(){
+        return driveMode;
+    }
+
+    @Override
+    public void driveDistance(double distanceInches, double xDirectionSpeed, double yDirectionSpeed) {
+
+    }
+
+    @Override
+    public void rotateDegrees(double angle, double angularSpeed) {
+
+    }
+
+    @Override
+    public void driveDistance(double xDirectionSpeed, double yDirectionSpeed, double distanceInches, Runnable completionRoutine) {
+        setCompletionRoutine(completionRoutine);
+    }
+
+    @Override
+    public void rotateDegrees(double relativeAngle, double angularSpeed, Runnable completionRoutine) {
+
+    }
+
+    @Override
+    public void driveManual(double xDirectionSpeed, double yDirectionSpeed) {
+        setCompletionRoutine(null);
+        driveManualImplementation(xDirectionSpeed, yDirectionSpeed);
+    }
+
+    private void driveManualImplementation(double xDirectionSpeed, double yDirectionSpeed) {
+        this.xDirectionSpeed = xDirectionSpeed;
+        this.yDirectionSpeed = yDirectionSpeed;
+        driveMode = DriveMode.DRIVERCONTROL;
+    }
+
+    private void stop() {
+        driveManualImplementation(0.0, 0.0);
+    }
+
+    @Override
+    public void lookAt(double angle, double speed) {
+        setCompletionRoutine(null);
+
+        desiredAngle = MathUtilities.normalizeAngle(angle);
+        rotationSpeed = speed;
+    }
+
+    @Override
+    public void maintainHeading() {
+        setCompletionRoutine(null);
+        desiredAngle = gyroscope.getYaw();
+        rotationSpeed = MAINTAINING_HEADING_SPEED;
+    }
+
+    private void setCompletionRoutine(Runnable completionRountime) {
+        if (currentCompletionRoutine != null) {
+            throw new IllegalStateException("Tried to perform a lift action while one was already in progress!");
+        }
+
+        currentCompletionRoutine = completionRountime;
     }
 
     @Override
     public void init() {
-        drive.init();
+        currentCompletionRoutine = null;
+        stop();
+        gyroscope.resetYaw();
     }
 
     @Override
     public void periodic() {
+        double leftSpeed;
+        double rightSpeed;
+        double middleSpeed;
 
-        drive.driveManual(getLeftStickX(), getLeftStickY());
+        // Linear Motion
+        double fieldAngle = Math.atan2(yDirectionSpeed, xDirectionSpeed) - (Math.PI / 2);
+        double robotDriveAngle = fieldAngle + gyroscope.getYaw();
 
-        double rightX = xboxController.getX(Hand.kRight);
-        double rightY = xboxController.getY(Hand.kRight);
-        double angle = (Math.atan2(rightY, rightX) + (Math.PI / 2));
-        double rotationSpeed = Math.sqrt(Math.pow(rightX, 2) + Math.pow(rightY, 2));
-        if (rotationSpeed < TOLERANCE) {
+        double speedMagnitude = Math.sqrt(Math.pow(xDirectionSpeed, 2) + Math.pow(yDirectionSpeed, 2));
+        leftSpeed = Math.cos(robotDriveAngle) * speedMagnitude * .7;
+        rightSpeed = leftSpeed;
+        middleSpeed = Math.sin(robotDriveAngle) * speedMagnitude;
 
-            rotationSpeed = 0;
-        }
-        else {
-            rotationSpeed = Math.pow(rotationSpeed, 1);
-        }
+        Debug.logPeriodic("---------------------------------------------");
+        //Debug.logPeriodic("controllerAngle: " + MathUtilities.normalizeAngle(fieldAngle));
+        Debug.logPeriodic("      gyroAngle: " + gyroscope.getYaw());
+        //Debug.logPeriodic("robotDriveAngle:" + MathUtilities.normalizeAngle(robotDriveAngle));
 
-        if(rotationSpeed < ROTATION_SPEED_THRESHOLD) {
-            if(!headingIsMaintained) {
-                drive.maintainHeading();
-                headingIsMaintained = true;
+        // Angular Motion
+        if (true) {
+            double deltaAngle = gyroscope.getYaw() - desiredAngle;
+
+            Debug.logPeriodic(" desiredAngle: " + desiredAngle);
+            Debug.logPeriodic(" deltaAngle1: " + deltaAngle);
+
+            if (Math.abs(deltaAngle) > Math.PI) {
+                deltaAngle -= (Math.PI * 2) * Math.signum(deltaAngle);
             }
-        } else {
-            drive.lookAt(angle, rotationSpeed);
-            headingIsMaintained = false;
+
+            Debug.logPeriodic(" deltaAngle2: " + deltaAngle);
+
+            double actualSpeed = rotationSpeed * Math.pow(Math.abs(deltaAngle) / Math.PI, DELTA_ANGLE_SPEED_POWER);
+            leftSpeed *= 1 - actualSpeed;
+            rightSpeed *= 1 - actualSpeed;
+
+            if (deltaAngle < 0) {
+                leftSpeed += actualSpeed;
+                rightSpeed -= actualSpeed;
+            }
+            else {
+                leftSpeed -= actualSpeed;
+                rightSpeed += actualSpeed;
+            }
+
+            Debug.logPeriodic(" actualSpeed: " + actualSpeed);
         }
 
-        if (xboxController.getBumper(Hand.kRight)) {
-            launcher.start();
-        } else {
-            launcher.stop();
-        }
-
-        if (xboxController.getYButton()) {
-            hook.grabPanel();
-        }
-
-        if (xboxController.getXButton()) {
-            hook.releasePanel();
-        }
-
-        if (xboxController.getBButton()) {
-            cargoGuide.engageGuide();
-        }
-
-        if(xboxController.getAButton()) {
-            cargoGuide.disengageGuide();
-        }
-    }
-
-    private double getLeftStickY() {
-        double leftY = xboxController.getY(Hand.kLeft);
-        return (Math.abs(leftY) < TOLERANCE) ? 0 : -Math.pow(leftY, CONTROL_EXPONENT);
-    }
-
-    private double getLeftStickX() {
-        double leftX = xboxController.getX(Hand.kLeft);
-        return (Math.abs(leftX) < TOLERANCE) ? 0 : Math.pow(leftX, CONTROL_EXPONENT);
+        // Set Motor Speeds
+        leftMotorControllerLead.set(ControlMode.PercentOutput, leftSpeed);
+        rightMotorControllerLead.set(ControlMode.PercentOutput, rightSpeed);
+        middleMotorControllerLead.set(ControlMode.PercentOutput, middleSpeed);
     }
 }
